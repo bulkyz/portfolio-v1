834b4e65eb761d2f3869a162ada12d8b28763ccb
bulkyz <vincent360@outlook.com>
2022-10-10 14:03:56 +0200

    design changes and js project title

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let projectText = document.getElementById('project-text');
+projectText.innerText = 'Projects';
+
+// Shows project name based on which project you hover on
+function showText1() {
+    projectText.innerText = 'Project 1'
+}
+
+function showText2() {
+    projectText.innerText = 'Project 2'
+}
+
+function showText3() {
+    projectText.innerText = 'Project 3'
+}
+
+function defaultText() {
+    projectText.innerText = 'Projects'
+}
