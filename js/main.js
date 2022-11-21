1e43e63d61f84c5a703b01ee52c3ca887702d474
bulkyz <vincent360@outlook.com>
2022-11-21 17:34:42 +0100

    bunch of redo

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 let projectText = document.getElementById('project-text');
 projectText.innerText = 'Projects';
 
-//cleanup code
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// Animation
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 
 // Shows project name based on which project you hover on
-function showText1() {
-    projectText.innerText = 'EatWell app (phone view needed)'
-}
-
-function showText2() {
-    projectText.innerText = 'API demo app (phone view needed)'
-}
-
-function showText3() {
-    projectText.innerText = 'JS game (desktop)'
-}
-
-function defaultText() {
-    projectText.innerText = 'Projects'
+function displayTitle(title) {
+    return projectText.innerText = title;
 }
